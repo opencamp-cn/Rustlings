ddee0c2b78eee5e46454e860b4a686c0c37926a6
Orbit <582126074@qq.com>
2025-01-28 19:07:29 +0800

    done 16/xxx

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,11 +13,15 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
-
+fn calculate_price_of_apples(number_apple: u32) -> u32 {
+    
+    if number_apple > 40 {
+        number_apple * 1
+    }else{
+        number_apple * 2
+    }
+}
 // Don't modify this function!
 #[test]
 fn verify_test() {
